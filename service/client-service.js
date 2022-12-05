ca9df64b121f3f803f4a0f8b5b67f318f83936cc
lucasbiagettiaWin <lucasbiagetti@gmail.com>
2022-12-04 22:57:53 -0300

    fetch

--- service/client-service.js
+++ service/client-service.js
@@ -1,4 +1,4 @@
-//backticks
+
 const crearNuevaLinea = (nombre, email) => {
   const linea = document.createElement("tr");
   const contenido = `
@@ -38,24 +38,7 @@ const table = document.querySelector("[data-table]");
 // Update - PUT/PATCH
 // Delete - DELETE
 
-const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    http.open("GET", "http://localhost:3000/perfiles");
-
-    http.send();
-
-    http.onload = () => {
-      const response = JSON.parse(http.response);
-      if (http.status >= 400) {
-        reject(response);
-      } else {
-        resolve(response);
-      }
-    };
-  });
-  return promise;
-};
+const listaClientes = () => fetch("http://localhost:3000/perfil").then (respuesta => respuesta.json());
 
 listaClientes()
   .then((data) => {

520ffea22a4df1b10d282f15a96f3c2bd666f3a1
lucasbiagettiaWin <lucasbiagetti@gmail.com>
2022-12-05 00:16:36 -0300

    fix

--- controllers/client-controller.js
+++ controllers/client-controller.js
@@ -1,39 +1,52 @@
 import { clientServices } from "../service/client-service.js";
 
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement("tr");
-    const contenido = `
-      <td class="td" data-td>
-        ${nombre}
-      </td>
-      <td>${email}</td>
-      <td>
-        <ul class="table__button-control">
-          <li>
-            <a
-              href="../screens/editar_cliente.html"
-              class="simple-button simple-button--edit"
-            >
-              Editar
-            </a>
-          </li>
-          <li>
-            <button class="simple-button simple-button--delete" type="button">
-              Eliminar
-            </button>
-          </li>
-        </ul>
-      </td>
-    `;
-    linea.innerHTML = contenido;
-    return linea;
-  };
+const crearNuevaLinea = (nombre, email, id) => {
+  const linea = document.createElement("tr");
+  const contenido = `
+    <td class="td" data-td>
+      ${nombre}
+    </td>
+    <td>${email}</td>
+    <td>
+      <ul class="table__button-control">
+        <li>
+          <a
+            href="../screens/editar_cliente.html?id=${id}"
+            class="simple-button simple-button--edit"
+          >
+            Editar
+          </a>
+        </li>
+        <li>
+          <button class="simple-button simple-button--delete" type="button" id="${id}">
+            Eliminar
+          </button>
+        </li>
+      </ul>
+    </td>
+  `;
+  linea.innerHTML = contenido;
+  const btn = linea.querySelector("button");
+  btn.addEventListener("click", () => {
+    const id = btn.id;
+    clientServices
+      .eliminarCliente(id)
+      .then((respuesta) => {
+        console.log(respuesta);
+      })
+      .catch((err) => alert("Ocurrió un error"));
+  });
 
+  return linea;
+};
 
-  listaClientes()
+const table = document.querySelector("[data-table]");
+
+clientServices
+  .listaClientes()
   .then((data) => {
-    data.forEach((perfil) => {
-      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+    data.forEach(({ nombre, email, id }) => {
+      const nuevaLinea = crearNuevaLinea(nombre, email, id);
       table.appendChild(nuevaLinea);
     });
   })
--- controllers/registro-controller.js
+++ controllers/registro-controller.js
@@ -1,11 +1,16 @@
 import { clientServices } from "../service/client-service.js";
-const formulario = document.querySelector("[data-form]");
-formulario.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const nombre = document.querySelector("[data-nombre]").value;
-    const email = document.querySelector("[data-nombre]").value;
-    clientServices.crearCliente (nombre, email).then(respuesta => {
-        window.location.href= "/screens/registro_completado.html"
 
-    }).catch ((err) => console.log(err));
+
+const formulario = document.querySelector("[data-form]");
+
+formulario.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const nombre = document.querySelector("[data-nombre]").value;
+  const email = document.querySelector("[data-email]").value;
+  clientServices
+    .crearCliente(nombre, email)
+    .then(() => {
+      window.location.href = "/screens/registro_completado.html";
+    })
+    .catch((err) => console.log(err));
 });

f3aa385a51f99b456e18b2ff545e84f2a705543d
lucasbiagettiaWin <lucasbiagetti@gmail.com>
2022-12-07 00:41:01 -0300

    ft/update

--- service/client-service.js
+++ service/client-service.js
@@ -16,10 +16,15 @@ const eliminarCliente = (id) => {
   });
 }
 
+const detalleCliente =(id)=>{
+  return fetch(`http://localhost:3000/perfil/${id}`).then (respuesta => respuesta.json());
+}
+
 
 
 export const clientServices = {
   listaClientes,
   crearCliente,
-  eliminarCliente 
+  eliminarCliente,
+  detalleCliente 
 }
